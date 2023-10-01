d2e932ec9f31d846ed9dca8e9cfc7600360b162f
K1tanaK0mbat <kkeayouth@gmail.com>
2023-10-01 12:43:21 -0500

    sql shows names instead of ids

--- database/query.sql
+++ database/query.sql
@@ -2,23 +2,20 @@
 SELECT
     roles.id AS role_id,
     roles.title AS role_title,
-    departments.dep_name AS dep_name,
+    roles.department_name AS dep_name,
     roles.salary
-FROM roles
-JOIN departments ON roles.department_id = departments.id;
+FROM roles;
 
 SELECT
-    e.id AS employee_id,
-    e.first_name AS employee_first_name,
-    e.last_name AS employee_last_name,
-    r.title AS employee_role_title,
-    d.dep_name AS employee_department_name,
-    r.salary AS employee_salary,
-    CONCAT(m.first_name, ' ', m.last_name) AS manager_name
-FROM employees e
-JOIN roles r ON e.role_id = r.id
-JOIN departments d ON r.department_id = d.id
-LEFT JOIN employees m ON e.manager_id = m.id;
+    employees.id AS employee_id,
+    employees.first_name AS employee_first_name,
+    employees.last_name AS employee_last_name,
+    employees.role_name AS employee_role_title,
+    employees.department_name AS employee_department_name,
+    employees.salary AS employee_salary,
+    CONCAT(managers.first_name, ' ', managers.last_name) AS manager_name
+FROM employees
+LEFT JOIN employees AS managers ON employees.manager_id = managers.id;
 
 
 
