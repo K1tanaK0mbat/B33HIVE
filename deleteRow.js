4c72520d39e7b82598cc83ea0230eb14c0a32131
K1tanaK0mbat <kkeayouth@gmail.com>
2023-10-01 16:39:07 -0500

    fixed delete functions pt3

--- deleteRow.js
+++ deleteRow.js
@@ -44,110 +44,106 @@ function eraseWorker(callback) {
 }
 
 function eraseDep(callback) {
-  const departments = 'SELECT dep_name FROM departments';
-
-  db.query(departments, (err, departments) => {
-    if (err) {
-      console.error(err);
-      return callback();
-    }
-    inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'departments',
-          message: 'Select a department to delete ',
-          choices: departments.map((department) => ({
-            name: department.dep_name,
-            value: department.id,
-          })),
-        },
-      ])
-      .then((answers) => {
-        const { departments } = answers;
-        const sql = 'DELETE FROM departments WHERE dep_name = ?';
-        db.query(sql, [departments], (err, result) => {
-          if (err) {
-            console.error(err);
-          } else {
-            console.log('Deleted department');
-          }
-          callback();
+    const departments = 'SELECT dep_name FROM departments';
+  
+    db.query(departments, (err, departments) => {
+      if (err) {
+        console.error(err);
+        return callback();
+      }
+      inquirer
+        .prompt([
+          {
+            type: 'list',
+            name: 'department',
+            message: 'Select a department to delete ',
+            choices: departments.map((department) => department.dep_name),
+          },
+        ])
+        .then((answers) => {
+          const { department } = answers;
+          const sql = 'DELETE FROM departments WHERE dep_name = ?';
+          db.query(sql, [department], (err, result) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log('Deleted department');
+            }
+            callback();
+          });
         });
-      });
-  });
-}
+    });
+  }
+  
 
-function eraseRole(callback) {
-  const roles = 'SELECT title FROM roles';
-
-  db.query(roles, (err, roles) => {
-    if (err) {
-      console.error(err);
-      return callback();
-    }
-    inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'role',
-          message: 'Select a role to delete ',
-          choices: roles.map((role) => ({
-            name: role.title,
-            value: role.id,
-          })),
-        },
-      ])
-      .then((answers) => {
-        const { role } = answers;
-        const sql = 'DELETE FROM roles WHERE title = ?';
-        db.query(sql, [role], (err, result) => {
-          if (err) {
-            console.error(err);
-          } else {
-            console.log('Deleted role');
-          }
-          callback();
+  function eraseRole(callback) {
+    const roles = 'SELECT title FROM roles';
+  
+    db.query(roles, (err, roles) => {
+      if (err) {
+        console.error(err);
+        return callback();
+      }
+      inquirer
+        .prompt([
+          {
+            type: 'list',
+            name: 'role',
+            message: 'Select a role to delete ',
+            choices: roles.map((role) => role.title),
+          },
+        ])
+        .then((answers) => {
+          const { role } = answers;
+          const sql = 'DELETE FROM roles WHERE title = ?';
+          db.query(sql, [role], (err, result) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log('Deleted role');
+            }
+            callback();
+          });
         });
-      });
-  });
-}
+    });
+  }
+  
 
 function Delete(callback) {
-  inquirer
-    .prompt([
-      {
-        type: 'list',
-        name: 'options',
-        message: 'Which do you want to delete?',
-        choices: [
-          'Delete an employee',
-          'Delete a department',
-          'Delete a role',
-          'Exit'
-        ],
-      },
-    ])
-    .then((answers) => {
-      switch (answers.options) {
-        case 'Delete an employee':
-          eraseWorker(() => Delete());
-          break;
-        case 'Delete a department':
-          eraseDep(() => Delete());
-          break;
-        case 'Delete a role':
-          eraseRole(() => Delete());
-          break;
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'options',
+          message: 'Which do you want to delete?',
+          choices: [
+            'Delete an employee',
+            'Delete a department',
+            'Delete a role',
+            'Exit'
+          ],
+        },
+      ])
+      .then((answers) => {
+        const { options } = answers;
+        switch (options) {
+          case 'Delete an employee':
+            eraseWorker(() => Delete(callback));
+            break;
+          case 'Delete a department':
+            eraseDep(() => Delete(callback));
+            break;
+          case 'Delete a role':
+            eraseRole(() => Delete(callback));
+            break;
           case 'Exit':
             if (callback) {
-                callback();
-              } else {
-                process.exit();
-              }
-              break;
-      }
-    });
-}
+              callback(); 
+            }
+            break;
+        }
+      });
+  }
+  
 
 module.exports = { Delete };
